2709141dc37ae68896f890165d377da42302e105
ashis <ashis@192.168.1.12>
2020-12-02 15:00:49 +0530

    First Commit

---
+++ App1/src/p1/DemoApp.java
@@ -0,0 +1,15 @@
+package p1;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		
+		new DemoApp().caller();
+		
+	}
+	
+	public void caller()
+	{
+		System.out.println(" caller is called");
+	}
+}
